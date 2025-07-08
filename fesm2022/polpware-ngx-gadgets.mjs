dc6e97351c7c3e0b9ef747b19d8c8152e8761ef6
Tomcat <polpware@gmail.com>
2025-07-08 07:50:36 -0400

    New publish

--- fesm2022/polpware-ngx-gadgets.mjs
+++ fesm2022/polpware-ngx-gadgets.mjs
@@ -1,5 +1,5 @@
 import * as i0 from '@angular/core';
-import { EventEmitter, Component, Input, Output, NgModule } from '@angular/core';
+import { EventEmitter, Component, Output, Input, NgModule } from '@angular/core';
 import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import * as i1 from '@angular/common';
 import { CommonModule } from '@angular/common';
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import * as i3 from '@fortawesome/angular-fontawesome';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+const _c0 = (a0, a1) => ({ "form-control-sm": a0, "form-control-lg": a1 });
 function LanguageSwitchComponent_option_3_Template(rf, ctx) { if (rf & 1) {
     i0.ɵɵelementStart(0, "option", 4);
     i0.ɵɵtext(1);
@@ -15,10 +16,9 @@ function LanguageSwitchComponent_option_3_Template(rf, ctx) { if (rf & 1) {
 } if (rf & 2) {
     const l_r1 = ctx.$implicit;
     i0.ɵɵpropertyInterpolate("value", l_r1.value);
-    i0.ɵɵadvance(1);
+    i0.ɵɵadvance();
     i0.ɵɵtextInterpolate(l_r1.text);
 } }
-const _c0 = function (a0, a1) { return { "form-control-sm": a0, "form-control-lg": a1 }; };
 class LanguageSwitchComponent {
     constructor() {
         this.faGlobe = faGlobe;
@@ -50,26 +50,28 @@ class LanguageSwitchComponent {
         this.changed.emit(this.langValue);
     }
     static { this.ɵfac = function LanguageSwitchComponent_Factory(t) { return new (t || LanguageSwitchComponent)(); }; }
-    static { this.ɵcmp = /*@__PURE__*/ i0.ɵɵdefineComponent({ type: LanguageSwitchComponent, selectors: [["polp-gadget-language-switch"]], inputs: { langOptions: "langOptions", initValue: "initValue", size: "size" }, outputs: { changed: "changed" }, features: [i0.ɵɵNgOnChangesFeature], decls: 4, vars: 7, consts: [[1, "form-inline"], [1, "me-1", 3, "icon"], [1, "form-control", 3, "ngClass", "ngModel", "ngModelChange", "change"], [3, "value", 4, "ngFor", "ngForOf"], [3, "value"]], template: function LanguageSwitchComponent_Template(rf, ctx) { if (rf & 1) {
+    static { this.ɵcmp = /*@__PURE__*/ i0.ɵɵdefineComponent({ type: LanguageSwitchComponent, selectors: [["polp-gadget-language-switch"]], inputs: { langOptions: "langOptions", initValue: "initValue", size: "size" }, outputs: { changed: "changed" }, features: [i0.ɵɵNgOnChangesFeature], decls: 4, vars: 7, consts: [[1, "form-inline"], [1, "me-1", 3, "icon"], [1, "form-control", 3, "ngModelChange", "change", "ngClass", "ngModel"], [3, "value", 4, "ngFor", "ngForOf"], [3, "value"]], template: function LanguageSwitchComponent_Template(rf, ctx) { if (rf & 1) {
             i0.ɵɵelementStart(0, "div", 0);
             i0.ɵɵelement(1, "fa-icon", 1);
             i0.ɵɵelementStart(2, "select", 2);
-            i0.ɵɵlistener("ngModelChange", function LanguageSwitchComponent_Template_select_ngModelChange_2_listener($event) { return ctx.langValue = $event; })("change", function LanguageSwitchComponent_Template_select_change_2_listener($event) { return ctx.change($event); });
+            i0.ɵɵtwoWayListener("ngModelChange", function LanguageSwitchComponent_Template_select_ngModelChange_2_listener($event) { i0.ɵɵtwoWayBindingSet(ctx.langValue, $event) || (ctx.langValue = $event); return $event; });
+            i0.ɵɵlistener("change", function LanguageSwitchComponent_Template_select_change_2_listener($event) { return ctx.change($event); });
             i0.ɵɵtemplate(3, LanguageSwitchComponent_option_3_Template, 2, 2, "option", 3);
             i0.ɵɵelementEnd()();
         } if (rf & 2) {
-            i0.ɵɵadvance(1);
+            i0.ɵɵadvance();
             i0.ɵɵproperty("icon", ctx.faGlobe);
-            i0.ɵɵadvance(1);
-            i0.ɵɵproperty("ngClass", i0.ɵɵpureFunction2(4, _c0, ctx.isSmallSize, ctx.isLargeSize))("ngModel", ctx.langValue);
-            i0.ɵɵadvance(1);
+            i0.ɵɵadvance();
+            i0.ɵɵproperty("ngClass", i0.ɵɵpureFunction2(4, _c0, ctx.isSmallSize, ctx.isLargeSize));
+            i0.ɵɵtwoWayProperty("ngModel", ctx.langValue);
+            i0.ɵɵadvance();
             i0.ɵɵproperty("ngForOf", ctx.langOptions);
         } }, dependencies: [i1.NgClass, i1.NgForOf, i2.NgSelectOption, i2.ɵNgSelectMultipleOption, i2.SelectControlValueAccessor, i2.NgControlStatus, i2.NgModel, i3.FaIconComponent], styles: ["[_nghost-%COMP%]   .form-inline[_ngcontent-%COMP%]{display:flex;flex-flow:row;align-items:center}"] }); }
 }
-(function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(LanguageSwitchComponent, [{
+(() => { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(LanguageSwitchComponent, [{
         type: Component,
         args: [{ selector: 'polp-gadget-language-switch', template: "<div class=\"form-inline\">\n    <fa-icon [icon]=\"faGlobe\" class=\"me-1\"></fa-icon>\n    <select class=\"form-control\"\n            [ngClass]=\"{'form-control-sm': isSmallSize, 'form-control-lg': isLargeSize}\"\n            [(ngModel)]=\"langValue\"\n            (change)=\"change($event)\">\n        <option *ngFor=\"let l of langOptions\" value=\"{{l.value}}\">{{l.text}}</option>    \n    </select>\n</div>\n", styles: [":host .form-inline{display:flex;flex-flow:row;align-items:center}\n"] }]
-    }], function () { return []; }, { langOptions: [{
+    }], () => [], { langOptions: [{
             type: Input
         }], initValue: [{
             type: Input
@@ -78,6 +80,7 @@ class LanguageSwitchComponent {
         }], changed: [{
             type: Output
         }] }); })();
+(() => { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassDebugInfo(LanguageSwitchComponent, { className: "LanguageSwitchComponent", filePath: "lib\\language-switch\\language-switch.component.ts", lineNumber: 9 }); })();
 
 class NgxGadgetsModule {
     static { this.ɵfac = function NgxGadgetsModule_Factory(t) { return new (t || NgxGadgetsModule)(); }; }
@@ -86,7 +89,7 @@ class NgxGadgetsModule {
             FormsModule,
             FontAwesomeModule] }); }
 }
-(function () { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(NgxGadgetsModule, [{
+(() => { (typeof ngDevMode === "undefined" || ngDevMode) && i0.ɵsetClassMetadata(NgxGadgetsModule, [{
         type: NgModule,
         args: [{
                 declarations: [LanguageSwitchComponent],
